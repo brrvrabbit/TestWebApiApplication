3515553470f252ddb330ac4cda4786591f9fad1d
brrvrabbit <Krol@DESKTOP-KSI8FVA>
2023-09-07 13:17:19 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/TestDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1
+{
+    public class TestDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = "MyDb.db" };
+            var connectionString = connectionStringBuilder.ToString();
+            var connection = new SqliteConnection(connectionString);
+
+            optionsBuilder.UseSqlite(connection);
+        }
+    }
+}
